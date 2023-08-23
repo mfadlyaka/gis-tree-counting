3598048ebd07a877ebef8d61f7c7df49c774add1
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2023-08-24 00:53:39 +0700

    prettier tailwindcss

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,9 +1,9 @@
 export default function Product() {
   return (
     <main className="pt-[65px]">
-      <div className="bg-[#D3CABE] flex flex-row">
-        <div className="w-2/3 flex flex-col justify-center gap-10">
-          <p className="w-[600px] pl-20 font-semibold text-4xl leading-relaxed">
+      <div className="flex flex-row bg-[#D3CABE]">
+        <div className="flex w-2/3 flex-col justify-center gap-10">
+          <p className="w-[600px] pl-20 text-4xl font-semibold leading-relaxed">
             Welcome to the Palm Tree WebGIS product menu
           </p>
           <p className="w-[700px] pl-40 text-xl leading-normal">
@@ -17,11 +17,11 @@ export default function Product() {
         </div>
       </div>
 
-      <div className="flex justify-center items-center">
-        <div className="mt-28 w-[400px] h-[100px] rounded-2xl flex items-center justify-center bg-neutral-800 gap-3 shadow-lg shadow-neutral-900/40 transition-all duration-300 hover:shadow-none">
+      <div className="flex items-center justify-center">
+        <div className="mt-28 flex h-[100px] w-[400px] items-center justify-center gap-3 rounded-2xl bg-neutral-800 shadow-lg shadow-neutral-900/40 transition-all duration-300 hover:shadow-none">
           <a
             href="https://detect.roboflow.com/?model=pohonsawit2&version=7&api_key=2PNqpkPLTX6glZUiAPGu"
-            className="text-decoration-none w-full text-center text-2xl px-4 py-2 text-white rounded-xl"
+            className="text-decoration-none w-full rounded-xl px-4 py-2 text-center text-2xl text-white"
           >
             Get Started
           </a>
